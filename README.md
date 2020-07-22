9557b22de536c9f736c9d56e8cced7962bc431cc
wangLijuan <528036177@qq.com>
2020-07-22 20:59:38 +0800

    优化路由和vuex

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-﻿<!--
+<!--
  * @Author: 
  * @Date: 2020-07-21 19:41:42
  * @LastEditors: wanglijuan01
- * @LastEditTime: 2020-07-21 20:26:15
+ * @LastEditTime: 2020-07-22 08:00:44
  * @Description: 
 --> 
 # wlj-cli
@@ -25,21 +25,19 @@ wlj i
 ## 1.0.0
 实现基本功能
 
-## 后续迭代
-### 1.1.0
-修改了生成后的package.json相关内容  
-增加对版本的提问  
+1.1.0
+修改了生成后的package.json相关内容
+增加对版本的提问
 在模板下载后自动执行npm i将依赖包下载到node_modules中
 
-
-### 2.0.0
-增加了ejs的模板渲染，修改index.html，package.json等文件的相关内容  
-增加了是否启用vuex的选择  
+2.0.0
+增加了ejs的模板渲染，修改index.html，package.json等文件的相关内容
+增加了是否启用vuex的选择
 增加了模板文件，在启用vuex的时候将模板文件写入
 
-### 2.1.0
-增加对App.vue的ejs模板处理  
-增加对UI框架的配置  
-增加对css预处理语言的配置  
+2.1.0
+增加对App.vue的ejs模板处理
+增加对UI框架的配置
+增加对css预处理语言的配置
 
 敬请期待。。。
--- command/init.js
+++ command/init.js
@@ -2,7 +2,7 @@
  * @Author: 
  * @Date: 2020-07-21 17:09:56
  * @LastEditors: wanglijuan01
- * @LastEditTime: 2020-07-21 20:24:30
+ * @LastEditTime: 2020-07-22 17:33:21
  * @Description: 
  */ 
 const inquirer = require('inquirer')
@@ -28,52 +28,52 @@ module.exports = () => {
     name: 'version',
     default: '1.0.0'
   },
-  // {
-  //   type: 'confirm',
-  //   message: 'use vueX?',
-  //   name: 'useVuex',
-  //   default: true
-  // },
-  // {
-  //   type: 'confirm',
-  //   message: 'use vue router?',
-  //   name: 'useVueRouter',
-  //   default: true
-  // },
-  // {
-  //   type : 'list',
-  //   message: 'the UI fragment:',
-  //   name: 'UIfrag',
-  //   choices: [{
-  //     name: 'no UI fragment', 
-  //     value: 'none'
-  //   },
-    // {
-    //   name: 'element-ui',
-    //   value: 'eleUI'
-    // }]
-  // },
-  // {
-  //   type: 'list',
-  //   message: 'the css preprocessing language:',
-  //   name: 'cssPrep',
-  //   choices: [
-  //     {
-  //       name: 'no preprocess', 
-  //       value: 'none'
-  //     },{
-  //       name: 'less',
-  //       value: 'less'
-  //     },{
-  //       name: 'sass',
-  //       value: 'sass'
-  //     }]
-  // }
+  {
+    type: 'confirm',
+    message: 'use vueX?',
+    name: 'useVuex',
+    default: true
+  },
+  {
+    type: 'confirm',
+    message: 'use vue router?',
+    name: 'useVueRouter',
+    default: true
+  },
+  {
+    type : 'list',
+    message: 'the UI fragment:',
+    name: 'UIfrag',
+    choices: [{
+      name: 'no UI fragment', 
+      value: 'none'
+    },
+    {
+      name: 'element-ui',
+      value: 'eleUI'
+    }]
+  },
+  {
+    type: 'list',
+    message: 'the css preprocessing language:',
+    name: 'cssPrep',
+    choices: [
+      {
+        name: 'no preprocess', 
+        value: 'none'
+      },{
+        name: 'less',
+        value: 'less'
+      },{
+        name: 'sass',
+        value: 'sass'
+      }]
+  }
 ]
   inquirer.prompt(prompts).then(answer => { // 通过用户的输入进行各种操作
     console.log(chalk.green('开始初始化文件\n'))
     console.log(chalk.gray('初始化中...'))
-    const gitUrl = 'https://github.com/wangAlisa/vue-elementui-initprogect.git'
+    const gitUrl = 'https://github.com/wangAlisa/vue-template.git'
     exec(`git clone ${gitUrl} ${answer.projectName}`, (error, stdout, stderr) => {
       console.log('模板下载完毕')
       if(error) { // 当有错误的时候打印出错误并退出操作
@@ -82,49 +82,51 @@ module.exports = () => {
         process.exit()
       }
       // 当配置vuex的时候进行的操作
-      // if(answer.useVuex) {
-      //   fs.mkdirSync(`${process.cwd()}/${answer.projectName}/src/store`)
-      //   fs.mkdirSync(`${process.cwd()}/${answer.projectName}/src/store/modules`)
-      //   fs.mkdirSync(`${process.cwd()}/${answer.projectName}/src/store/modules/module`)
-      //   let moduleFiles = ['index.js', 'modules/module/actions.js', 'modules/module/getters.js','modules/module/index.js', 'modules/module/mutations.js','modules/module/state.js']
-      //   moduleFiles.forEach(val => {
-      //     let fileData = fs.readFileSync(__dirname+`/../templates/vuex/${val}`)
-      //     fs.writeFileSync(`${process.cwd()}/${answer.projectName}/src/store/${val}`, fileData)
-      //   })
-      //   console.log(chalk.green('vuex配置完成'));
-      // }
+      if(answer.useVuex) {
+        fs.mkdirSync(`${process.cwd()}/${answer.projectName}/src/store`)
+        fs.mkdirSync(`${process.cwd()}/${answer.projectName}/src/store/modules`)
+        fs.mkdirSync(`${process.cwd()}/${answer.projectName}/src/store/modules/module`)
+        let moduleFiles = ['index.js', 'modules/module/actions.js', 'modules/module/getters.js','modules/module/index.js', 'modules/module/mutations.js','modules/module/state.js']
+        moduleFiles.forEach(val => {
+          let fileData = fs.readFileSync(__dirname+`/../templates/vuex/${val}`)
+          fs.writeFileSync(`${process.cwd()}/${answer.projectName}/src/store/${val}`, fileData)
+        })
+        console.log(chalk.green('vuex配置完成'));
+      }
       // 当配置vue-router时进行操作
-      // if(answer.useVueRouter) {
-      //   fs.mkdirSync(`${process.cwd()}/${answer.projectName}/src/router`)
-      //   let moduleFiles = ['router/index.js']
-      //   moduleFiles.forEach(val => {
-      //     let fileData = fs.readFileSync(__dirname+ `/../templates/vue-router/${val}`)
-      //     fs.writeFileSync(`${process.cwd()}/${answer.projectName}/src/${val}`, fileData)
-      //   })
-      //   console.log(chalk.green('vue-router配置完成'))
-      // }
-      // let files = ['public/index.html', 'src/App.vue', 'src/main.js', 'package-lock.json', 'package.json']
-      // new Promise(resolve => {
-      //   files.forEach((val, index) => {
-      //     ejs.renderFile(`${answer.projectName}/${val}`, answer, (err, str) => {
-      //       fs.writeFileSync(`${answer.projectName}/${val}`, str, () => {
-      //         if(index === files.length - 1) {
-      //           resolve()
-      //         }
-      //       })
-      //     })
-      //   })
-      // }).then(() => {
-      //   exec(`cd ${projectName} && npm i`, (err, stdout, stderr) => {
-      //     console.log(chalk.green('依赖包下载完毕'))
-      //     if(err) {
-      //       console.log(chalk.red('拷贝文件失败'))
-      //       process.exit()
-      //     }
-      //     console.log(chalk.green('初始化完成'))
-      //     process.exit()
-      //   })
-      // })
+      if(answer.useVueRouter) {
+        fs.mkdirSync(`${process.cwd()}/${answer.projectName}/src/router`)
+        let moduleFiles = ['router/index.js']
+        moduleFiles.forEach(val => {
+          let fileData = fs.readFileSync(__dirname+ `/../templates/vue-router/${val}`)
+          fs.writeFileSync(`${process.cwd()}/${answer.projectName}/src/${val}`, fileData)
+        })
+        console.log(chalk.green('vue-router配置完成'))
+      }else{
+        exec(`rm -rf ${answer.projectName}/src/views`)
+      }
+      let files = ['public/index.html','src/App.vue','src/main.js','package.json', 'README.md']
+      new Promise(resolve=>{
+          files.forEach((val,index)=>{
+              ejs.renderFile(`${answer.projectName}/${val}`,answer,(err,str)=>{
+                  fs.writeFile(`${answer.projectName}/${val}`,str,()=>{
+                      if(index===files.length-1){
+                          resolve()
+                      }
+                  })
+              })
+          })
+      }).then(()=>{
+          exec(`cd ${answer.projectName} && npm install`,(err,stdout,stderr)=>{
+              console.log(chalk.green('依赖包下载完毕'))
+              if (error) { // 当有错误时打印出错误并退出操作
+                  console.log(chalk.red('拷贝文件失败'))
+                  process.exit()
+              }
+              console.log(chalk.green('初始化完成'))
+              process.exit() // 退出这次命令行操作
+          })
+      })
     })
   })
 }
